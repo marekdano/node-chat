a3b1620613058408a4fb011cd7782188a78477c2
Marek Dano <mk.dano@gmail.com>
2014-06-09 07:12:41 +0100

    added the simple sending and displaying the message from the clients

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
+// This is the main file of the chat app. It initializes a new express.js instance, all dependencies and listens on the port. App can be started by running "node app.js" in the terminal.
 
-/**
- * Module dependencies.
- */
+var express = require('express'),
+    routes = require('./routes'),
+    user = require('./routes/user'),
+    http = require('http'),
+    path = require('path');
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var PORT =  process.env.PORT || 3030;
 
-var app = express();
+var app = express()
+  , server = http.Server(app);
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+app.configure(function (){
+  app.set('port',PORT);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
@@ -30,6 +30,23 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+// Initialize a new socket.io object. It is bound to 
+// the express app, which allows them to coexist.
+var io = require('socket.io')(server);
+
+server.listen(app.get('port'), function (){
+    console.log("Express server listening on port " + app.get('port'));
 });
+
+// Define chatroom
+
+// usernames currently connected to the chatroom
+var usernames = {},
+    numUsers = 0;
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
